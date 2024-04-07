369e43fd05552593642176b0e491ad298a65754b
taku10101 <taku1011ht@gmail.com>
2024-04-07 14:31:44 +0900

    fix path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import { Button, Link } from "@yamada-ui/react";
 export default function Home() {
   return (
     <>
-    <div>トップページ(仮)</div>
-    <Link href="/category">
-      <Button variant="solid">単語帳一覧はコチラ</Button>
-    </Link>
+      <div>トップページ(仮)</div>
+      <Link href='/categories'>
+        <Button variant='solid'>単語帳一覧はコチラ</Button>
+      </Link>
     </>
   );
 }
