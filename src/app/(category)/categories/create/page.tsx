a41ea8bc2ea94351a6574328bf309d6da4156147
taku10101 <taku1011ht@gmail.com>
2024-04-07 14:22:56 +0900

    fix directly

--- src/app/(category)/categories/create/page.tsx
+++ src/app/(category)/categories/create/page.tsx
@@ -1,14 +1,24 @@
-import PageTitle from "@/component/pageTitle";
+import PageTitle from "@/app/_components/pageTitle";
 import { Box, Button, Input } from "@yamada-ui/react";
 
 export default function CreateGroup() {
-    return (
-        <>
-        <PageTitle title="グループを作成する" />
-        <Box w="80%" margin="100px auto 0 auto" display="flex" flexDirection="column" justifyContent="center" alignItems="center" gap="50px">
-            <Input variant="outline" placeholder="グループ名を入力"  size="lg"/>
-            <Button variant="solid" bg="sky.300">作成</Button>
-        </Box>
-        </>
-    );
+  return (
+    <>
+      <PageTitle title='グループを作成する' />
+      <Box
+        w='80%'
+        margin='100px auto 0 auto'
+        display='flex'
+        flexDirection='column'
+        justifyContent='center'
+        alignItems='center'
+        gap='50px'
+      >
+        <Input variant='outline' placeholder='グループ名を入力' size='lg' />
+        <Button variant='solid' bg='sky.300'>
+          作成
+        </Button>
+      </Box>
+    </>
+  );
 }
--- src/app/(category)/categories/page.tsx
+++ src/app/(category)/categories/page.tsx
@@ -1,28 +1,68 @@
-import GroupCard from '@/component/groupCard'
-import { Avatar, Box, Center, Link, Text } from '@yamada-ui/react'
-import { Icon as FontAwesomeIcon } from "@yamada-ui/fontawesome"
-import { faPlus } from "@fortawesome/free-solid-svg-icons"
-import React from 'react'
-import PageTitle from '../../../component/pageTitle'
+import GroupCard from "@/app/(category)/categories/_components/groupCard";
+import { Avatar, Box, Center, Link, Text } from "@yamada-ui/react";
+import { Icon as FontAwesomeIcon } from "@yamada-ui/fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import PageTitle from "../../_components/pageTitle";
 
 const page = () => {
-    return (
-        <>
-        <PageTitle title="保存済みのグループカード一覧 (NaN件)" />
-        <Box display="flex" flexWrap="wrap" gap="20px" pl="20px">
-            <GroupCard />
-            <GroupCard />
-            <GroupCard />
-            <GroupCard />
-            <GroupCard />
-            <GroupCard />
-        </Box>
+  const data = [
+    {
+      id: 1,
+      name: "グループ名",
+      description: "説明説明説明説明説明説明説明説明説明説明説明説明",
+    },
+    {
+      id: 2,
+      name: "グループ名",
+      description: "説明説明説明説明説明説明説明説明説明説明説明",
+    },
+    {
+      id: 3,
+      name: "グループ名",
+      description: "説明説明説明説明説明説明説明説明説明説明説明",
+    },
+    {
+      id: 4,
+      name: "グループ名",
+      description: "説明説明説明説明説明説明説明説明説明説明説明",
+    },
+    {
+      id: 5,
+      name: "グループ名",
+      description: "説明説明説明説明説明説明説明説明説明説明説明",
+    },
+    {
+      id: 6,
+      name: "グループ名",
+      description: "説明説明説明説明説明説明説明説明説明説明説明",
+    },
+  ];
+  return (
+    <>
+      <PageTitle title='保存済みのグループカード一覧 (NaN件)' />
+      <Box display='flex' flexWrap='wrap' gap='20px' pl='20px'>
+        {data.map((d) => (
+          <GroupCard key={d.id} />
+        ))}
+      </Box>
 
-        <Link href="/categories/create">
-            <Avatar bg="sky.300" size="lg" style={{position: "fixed", bottom: "3vh", right: "3vw", zIndex: 100, cursor: "pointer"}} icon={<FontAwesomeIcon icon={faPlus} />} />
-        </Link>
-        </>
-    )
-}
+      <Link href='/categories/create'>
+        <Avatar
+          bg='sky.300'
+          size='lg'
+          style={{
+            position: "fixed",
+            bottom: "3vh",
+            right: "3vw",
+            zIndex: 100,
+            cursor: "pointer",
+          }}
+          icon={<FontAwesomeIcon icon={faPlus} />}
+        />
+      </Link>
+    </>
+  );
+};
 
-export default page
+export default page;
