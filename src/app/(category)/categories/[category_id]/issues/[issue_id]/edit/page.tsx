0e47fd001476aeb7a16f51ad98d75c0add922228
dango-the-world <z.rohaso@gmail.com>
2024-04-09 17:09:43 +0900

    add edit form

---
+++ src/app/(category)/categories/[category_id]/issues/[issue_id]/edit/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { IssueForm } from "@/app/(category)/categories/[category_id]/issues/create/_components/IssueForm";
+import { Box, Center } from "@yamada-ui/react";
+import PageTitle from "@/app/_components/pageTitle";
+
+const page = () => {
+  return (
+    <>
+      <Center>
+        <Box w="1000px" m="20px">
+          <PageTitle title="問題編集" />
+          <IssueForm />
+        </Box>
+      </Center>
+    </>
+  );
+};
+
+export default page;
