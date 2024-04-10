6f59570cf4485f5d70e7be7da33959d675efc92c
xBlackTea <xBlackTea.Official@gmail.com>
2024-04-10 12:10:12 +0900

    add stickyNoteColor

--- src/app/(category)/categories/_components/groupCard.tsx
+++ src/app/(category)/categories/_components/groupCard.tsx
@@ -17,36 +17,36 @@ export default function GroupCard(props: GroupCardProps) {
 
   return (
     <>
-      <Card w={w} h='250px' bg='#eee'>
+      <Card w={w} h='240px' bg='#fffeee' border='solid 1px #ddd'>
         <Flex>
           {/* 付箋が減る場合、1枚当たり-40pxで調整 xBlackTea */}
           <Box w='calc(100% - 155px)'>
             <CardHeader>
-              <Text lineClamp={2} as='b' >
+              <Text pt='12px' lineClamp={1} as='b' >
                 {props.name}
               </Text>
             </CardHeader>
           </Box>
           <Box h='80px'>
             <Box display='flex' pr='auto' g='10px'>
-              <Box zIndex='100' w='30px' h='85px' pt='5px' mt='5px' rounded='3px' bg='#ff001a' color='white' fontSize='20px' textAlign='center' shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px'>
+              <Box zIndex='100' w='30px' h='85px' pt='5px' mt='5px' rounded='3px' bg='#FF6060' color='white' fontSize='20px' textAlign='center' shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px'>
                 <p>1</p>
               </Box>
-              <Box zIndex='100' w='30px' h='70px' pt='5px' mt='20px' rounded='3px' bg='#ff7300' color='white' fontSize='20px' textAlign='center' shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px'>
+              <Box zIndex='100' w='30px' h='70px' pt='5px' mt='20px' rounded='3px' bg='#FFa070' color='white' fontSize='20px' textAlign='center' shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px'>
                 <p>2</p>
               </Box>
-              <Box zIndex='100' w='30px' h='55px' pt='5px' mt='35px' rounded='3px' bg='#4fe000' color='white' fontSize='20px' textAlign='center' shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px'>
+              <Box zIndex='100' w='30px' h='55px' pt='5px' mt='35px' rounded='3px' bg='#10E070' color='white' fontSize='20px' textAlign='center' shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px'>
                 <p>8</p>
               </Box>
-              <Box zIndex='100' w='30px' h='40px' pt='5px' mt='50px' rounded='3px' bg='#00e2e6' color='white' fontSize='20px' textAlign='center' shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px'>
+              <Box zIndex='100' w='30px' h='40px' pt='5px' mt='50px' rounded='3px' bg='#00B0FF' color='white' fontSize='20px' textAlign='center' shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px'>
                 <p>29</p>
               </Box>
             </Box>
           </Box>
         </Flex>
-        <Card h='200px' gap='20px' bg='#ddd'>
+        <Card zIndex='50' h='200px' gap='20px' bg='#fffccc'>
           <CardBody gap='20px'>
-            <Text lineClamp={3}>{props.description}</Text>
+            <Text lineClamp={2} pt='12px' color='#555'>{props.description}</Text>
             <Box w='full' display='flex' justifyContent='space-around' alignItems='center'>
               <Link href={`/categories/${props.d}/issues/1/edit`}>
                 <Button bg='#fff' variant='solid'>編集</Button>
