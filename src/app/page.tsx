fdb6f299bbdcee685cefba3e39b41546b7367a5b
xBlackTea <xBlackTea.Official@gmail.com>
2024-04-10 18:13:14 +0900

    add welcome page not finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,61 +1,63 @@
-import { Button, Link, Text, Heading, Center, Box, Flex } from "@yamada-ui/react";
+import { Button, Link, Text, Heading, Center, Box, Flex, Card } from "@yamada-ui/react";
 
 export default function Home() {
   return (
     <>
-      <Box h='5vh' pl='xl' bg='#ddd'>
-        ロゴ{/* ロゴを入れないといけない xBlackTea */}
+      <Box h='5vh' pl='5vw' bg='#fff'>
       </Box>
-      <Box h='35vh' bg='#eee'>
-        <Flex>
-          <Box w='1395px'>
-            <Heading as='h1' size='4xl' p='xl' isTruncated>
-              目に見える忘却曲線。
-            </Heading>
-            <Text bgGradient='linear(to-l, #7928CA, #FF0080)' bgClip='text' as='b' fontSize='3xl' pl='xl' isTruncated>
+      <Box pl='5vw' pr='5vw' bg='#fff'>
+        <Box h='60vh' bg='#fff'>
+          <Center>
+            <Text as='b' fontSize='80px' pt='5vh' pb='5vh' isTruncated>
+              目に見える忘却曲線。<br/>
+            </Text>
+          </Center>
+          <Center>
+            <Text bgGradient='linear(to-l, #7928CA, #FF0080)' bgClip='text' as='b' fontSize='3xl' isTruncated>
               1, 2, 8, 29
             </Text>
             <Text as='b' fontSize='3xl' isTruncated>
               日ごとに、忘却曲線に挑戦して単語を覚える新感覚アプリが登場!<br/>
             </Text>
-
-            <Text bgGradient='linear(to-l, #7928CA, #FF0080)' bgClip='text' as='b' fontSize='3xl' pl='xl' isTruncated>
+          </Center>
+          <Center>
+            <Text bgGradient='linear(to-l, #7928CA, #FF0080)' bgClip='text' as='b' fontSize='3xl' isTruncated>
               色とりどりの付箋
             </Text>
             <Text as='b' fontSize='3xl' isTruncated>
               が、学習の進捗を視覚的に示し、一目で確認できます!<br/>
             </Text>
-
-            <Link href='/categories' pl='xl'>
-              <Button variant='solid'>単語帳一覧はコチラ!</Button>
-            </Link>
-          </Box>
-          <Box mt='12px'>
-            <Text pl='170px' as='b' fontSize='xl' isTruncated>
-              付箋をクリック!{/* これだとダサいから変更が必要 xBlackTea */}
-            </Text>
-            <Center p='20px' g='md'>
-              <Box w='100px' h='300px' pt='10px' rounded='md' bg='#ff001a' color='white' fontSize='48px' textAlign='center' shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px, rgba(0, 0, 0, 0.4) 0px 10px 15px'>
+          </Center>
+          {/* <Link href='/categories'>
+            <Button variant='solid'>単語帳一覧はコチラ!</Button>
+          </Link> */}
+        </Box>
+        <Box h='25vh' roundedTopRight='10px' roundedTopLeft='10px' bg='#fffeee'>
+          <Flex pt='calc(25vh - 230px)'>
+            <Box w='60vw'>
+              <Link href='/categories'>
+                <Text as='b' fontSize='48px' color='#000' pl='300px' decoration='none' isTruncated>
+                  単語帳一覧はコララから!
+                </Text>
+              </Link>
+            </Box>
+            <Flex minW='450px' pl='calc(30vw - 510px)' g='30px'>
+              <Box zIndex='400' w='100px' h='250px' pt='10px' rounded='md' bg='#FF6060' color='white' fontSize='48px' textAlign='center' shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px, rgba(0, 0, 0, 0.4) 0px 10px 15px'>
                 <p>1</p>
               </Box>
-              <Box w='100px' h='250px' pt='10px' mb='-50px' rounded='md' bg='#ff7300' color='white' fontSize='48px' textAlign='center' shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px, rgba(0, 0, 0, 0.4) 0px 10px 15px'>
+              <Box zIndex='300' w='100px' h='200px' pt='10px' mt='50px' rounded='md' bg='#FFa070' color='white' fontSize='48px' textAlign='center' shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px, rgba(0, 0, 0, 0.4) 0px 10px 15px'>
                 <p>2</p>
               </Box>
-              <Box w='100px' h='200px' pt='10px' mb='-100px' rounded='md' bg='#4fe000' color='white' fontSize='48px' textAlign='center' shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px, rgba(0, 0, 0, 0.4) 0px 10px 15px'>
+              <Box zIndex='200' w='100px' h='150px' pt='10px' mt='100px' rounded='md' bg='#10E070' color='white' fontSize='48px' textAlign='center' shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px, rgba(0, 0, 0, 0.4) 0px 10px 15px'>
                 <p>8</p>
               </Box>
-              <Box w='100px' h='150px' pt='10px' mb='-150px' rounded='md' bg='#00e2e6' color='white' fontSize='48px' textAlign='center' shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px, rgba(0, 0, 0, 0.4) 0px 10px 15px'>
+              <Box zIndex='400' w='100px' h='100px' pt='10px' mt='150px' rounded='md' bg='#00B0FF' color='white' fontSize='48px' textAlign='center' shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px, rgba(0, 0, 0, 0.4) 0px 10px 15px'>
                 <p>29</p>
               </Box>
-            </Center>
-          </Box>
-        </Flex>
-      </Box>
-        {/* 付箋をclickすることでここの中身に変化を与える xBlackTea */}
-      <Box h='60vh' pl='xl' pr='xl' pt='20px' bg='#ddd'>
-        <Box w='100%' h='80px' rounded='md' textAlign='center' bg='#ccc' shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px'>
+            </Flex>
+          </Flex>
         </Box>
-        <Box w='100%' h='460px' mt='20px' rounded='md' bg='#ccc'shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px'>
+        <Box h='10vh' roundedTopRight='10px' roundedTopLeft='10px' bg='#fffccc'>
         </Box>
       </Box>
     </>
