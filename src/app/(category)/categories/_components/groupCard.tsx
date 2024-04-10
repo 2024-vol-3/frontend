7b669d14b1fc5c6cdb6699d9d97e1ace169eaf89
xBlackTea <xBlackTea.Official@gmail.com>
2024-04-10 11:16:55 +0900

    add stickyNote and text

--- src/app/(category)/categories/_components/groupCard.tsx
+++ src/app/(category)/categories/_components/groupCard.tsx
@@ -1,15 +1,5 @@
 "use client";
-import {
-  Box,
-  Button,
-  Card,
-  CardBody,
-  CardHeader,
-  Indicator,
-  Link,
-  Text,
-  useBreakpointValue,
-} from "@yamada-ui/react";
+import {Box, Button, Card, CardBody, CardHeader, Indicator, Link, Text, useBreakpointValue, Center, Flex} from "@yamada-ui/react";
 
 type GroupCardProps = {
   name: string;
@@ -27,30 +17,48 @@ export default function GroupCard(props: GroupCardProps) {
 
   return (
     <>
-      <Card w={w} h='220px' gap='20px' bg='neutral.200'>
-        <CardHeader>
-          <Text isTruncated width='100%' as='b'>
-            {props.name}
-          </Text>
-        </CardHeader>
-        <CardBody gap='20px'>
-          <Text lineClamp={3}>{props.description}</Text>
-          <Box
-            w='full'
-            display='flex'
-            justifyContent='space-around'
-            alignItems='center'
-          >
-            <Link href={`/categories/${props.d}/issues/1/edit`}>
-              <Button variant='outline'>編集</Button>
-            </Link>
-            <Indicator showZero={false} withBorder label={90} variant='solid'>
-              <Link href={`/categories/${props.d}/solve`}>
-                <Button variant='solid'>解く</Button>
-              </Link>
-            </Indicator>
+      <Card w={w} h='250px' bg='#eee'>
+        <Flex>
+          {/* 付箋が減る場合、1枚当たり-40pxで調整 xBlackTea */}
+          <Box w='calc(100% - 155px)'>
+            <CardHeader>
+              <Text lineClamp={2} as='b' >
+                {props.name}
+              </Text>
+            </CardHeader>
           </Box>
-        </CardBody>
+          <Box h='80px'>
+            <Box display='flex' pr='auto' g='10px'>
+              <Box zIndex='100' w='30px' h='85px' pt='5px' mt='5px' rounded='3px' bg='#ff001a' color='white' fontSize='20px' textAlign='center' shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px'>
+                <p>1</p>
+              </Box>
+              <Box zIndex='100' w='30px' h='70px' pt='5px' mt='20px' rounded='3px' bg='#ff7300' color='white' fontSize='20px' textAlign='center' shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px'>
+                <p>2</p>
+              </Box>
+              <Box zIndex='100' w='30px' h='55px' pt='5px' mt='35px' rounded='3px' bg='#4fe000' color='white' fontSize='20px' textAlign='center' shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px'>
+                <p>8</p>
+              </Box>
+              <Box zIndex='100' w='30px' h='40px' pt='5px' mt='50px' rounded='3px' bg='#00e2e6' color='white' fontSize='20px' textAlign='center' shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px'>
+                <p>29</p>
+              </Box>
+            </Box>
+          </Box>
+        </Flex>
+        <Card h='200px' gap='20px' bg='#ddd'>
+          <CardBody gap='20px'>
+            <Text lineClamp={3}>{props.description}</Text>
+            <Box w='full' display='flex' justifyContent='space-around' alignItems='center'>
+              <Link href={`/categories/${props.d}/issues/1/edit`}>
+                <Button bg='#fff' variant='solid'>編集</Button>
+              </Link>
+              <Indicator showZero={false} withBorder label={90} variant='solid'>
+                <Link href={`/categories/${props.d}/solve`}>
+                  <Button bg='#fff' variant='solid'>解く</Button>
+                </Link>
+              </Indicator>
+            </Box>
+          </CardBody>
+        </Card>
       </Card>
     </>
   );
