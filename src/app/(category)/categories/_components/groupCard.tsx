ea63a9fe5c437f70fb9bef04786f85f0e7183f01
xBlackTea <xBlackTea.Official@gmail.com>
2024-04-12 08:00:37 +0900

    add StickyPaper components

--- src/app/(category)/categories/_components/groupCard.tsx
+++ src/app/(category)/categories/_components/groupCard.tsx
@@ -1,4 +1,5 @@
 "use client";
+import StickyPaper from "@/app/(category)/categories/_components/stickyPaper";
 import {Box, Button, Card, CardBody, CardHeader, Indicator, Link, Text, useBreakpointValue, Center, Flex} from "@yamada-ui/react";
 
 type GroupCardProps = {
@@ -27,22 +28,7 @@ export default function GroupCard(props: GroupCardProps) {
               </Text>
             </CardHeader>
           </Box>
-          <Box h='80px'>
-            <Box display='flex' pr='auto' g='10px'>
-              <Box zIndex='100' w='30px' h='85px' pt='5px' mt='5px' rounded='3px' bg='#FF6060' color='white' fontSize='20px' textAlign='center' shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px'>
-                <p>1</p>
-              </Box>
-              <Box zIndex='100' w='30px' h='70px' pt='5px' mt='20px' rounded='3px' bg='#FFa070' color='white' fontSize='20px' textAlign='center' shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px'>
-                <p>2</p>
-              </Box>
-              <Box zIndex='100' w='30px' h='55px' pt='5px' mt='35px' rounded='3px' bg='#10E070' color='white' fontSize='20px' textAlign='center' shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px'>
-                <p>8</p>
-              </Box>
-              <Box zIndex='100' w='30px' h='40px' pt='5px' mt='50px' rounded='3px' bg='#00B0FF' color='white' fontSize='20px' textAlign='center' shadow='rgba(0, 0, 0, 0.1) 0px 0px 0px, rgba(0, 0, 0, 0.2) 0px 5px 10px'>
-                <p>29</p>
-              </Box>
-            </Box>
-          </Box>
+          <StickyPaper/>
         </Flex>
         <Card zIndex='50' h='200px' gap='20px' bg='#fffccc'>
           <CardBody gap='20px'>
