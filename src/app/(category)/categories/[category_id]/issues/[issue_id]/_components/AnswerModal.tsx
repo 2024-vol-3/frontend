197b721c1eca53359146b766a18f6f8982acf73e
hinata <atahin0607@gmail.com>
2024-04-13 14:58:25 +0900

    遷移修正、フォーム消去

--- src/app/(category)/categories/[category_id]/issues/[issue_id]/_components/AnswerModal.tsx
+++ src/app/(category)/categories/[category_id]/issues/[issue_id]/_components/AnswerModal.tsx
@@ -1,6 +1,4 @@
-import { Avatar, Button, Modal, ModalBody, ModalFooter, ModalHeader } from "@yamada-ui/react"
-import { Icon as FontAwesomeIcon } from "@yamada-ui/fontawesome";
-import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { Button, Center, Modal, ModalBody, ModalFooter, ModalHeader } from "@yamada-ui/react"
 import { useModal } from "../hooks/useModal";
 import { useParams, useRouter } from "next/navigation";
 
@@ -12,17 +10,17 @@ const AnswerModal = () => {
     const { category_id, issue_id } = params;
 
     const handleClosse = () : void => {
-        router.push(`/categories/${category_id}/${Number(issue_id) + 1}`);
+        router.push(`/categories/${category_id}/issues/${Number(issue_id) + 1}`);
         setIsOpen(false);
     }
 
     return (
         <>
         <Modal isOpen={isOpen} onClose={handleClosse}>
-            <ModalHeader>正解！</ModalHeader>
+            <ModalHeader p="40px 30px 0 30px" textAlign="center">{`覚えられましたか？\nあと1日後にもう一度挑戦！`}</ModalHeader>
 
             <ModalBody>
-                <Avatar size='xl' bg="green.300" icon={<FontAwesomeIcon icon={faCheck} />} style={{margin: "30px auto"}}/>
+                <Center w="120px" h="120px" m="30px auto" bg="green.300" borderRadius="50%" color="#fff" fontSize="1.8rem" fontWeight="bold">1日後</Center>
             </ModalBody>
 
             <ModalFooter>
--- src/app/(category)/categories/[category_id]/issues/[issue_id]/_components/SolveForm.tsx
+++ src/app/(category)/categories/[category_id]/issues/[issue_id]/_components/SolveForm.tsx
@@ -1,7 +1,6 @@
 "use client"
 import { Accordion, AccordionItem, Button, Card, CardBody, CardHeader, Flex, FormControl, Heading, Text, Textarea, VStack } from '@yamada-ui/react'
 import React from 'react'
-import { useForm, SubmitHandler } from "react-hook-form";
 import { SolveType } from './solveType';
 import { useParams } from 'next/navigation';
 import Link from 'next/link';
@@ -10,18 +9,17 @@ import { useModal } from '../hooks/useModal';
 
 const SolveForm = () => {
 
-    const { register, handleSubmit, formState: { errors } } = useForm<SolveType>();
-    const { isOpen, setIsOpen } = useModal();
+    const { setIsOpen } = useModal();
     const params = useParams();
     const { category_id, issue_id } = params;
 
-    const onSubmit: SubmitHandler<SolveType> = (data) => {
+    const handleNext = () : void => {
         setIsOpen(true);
-    };
+    }
 
     return (
         <>
-        <VStack as="form" onSubmit={handleSubmit(onSubmit)}>
+        <VStack>
 
             <Card>
                 <CardHeader>
@@ -34,10 +32,6 @@ const SolveForm = () => {
                     </Text>
                 </CardBody>
 
-                <FormControl w="95%" m="30px auto" isInvalid={!!errors.answer} errorMessage={errors.answer?.message}>
-                    <Textarea h="150px" placeholder="回答を入力してください" {...register("answer", {required: {value: true, message: "回答を入力してください"}})}/>
-                </FormControl>
-
                 <Accordion variant="card" isToggle>
                     <AccordionItem label="解説を表示">
                         答えがここに表示されます。答えがここに表示されます。答えがここに表示されます。答えがここに表示されます。
@@ -46,12 +40,12 @@ const SolveForm = () => {
             </Card>
 
             <Flex justify="space-around" marginTop="20px">
-                <Link href={issue_id === "1" ? "/categories" : `/categories/${category_id}/${Number(issue_id) - 1}`}>
+                <Link href={issue_id === "1" ? "/categories" : `/categories/${category_id}/issues/${Number(issue_id) - 1}`}>
                     <Button w="200px">
                     {issue_id === "1" ? "カテゴリー一覧" : "前の問題"}
                     </Button>
                 </Link>
-                <Button w="200px" type="submit">
+                <Button w="200px" onClick={handleNext}>
                     回答を送信
                 </Button>
             </Flex>
